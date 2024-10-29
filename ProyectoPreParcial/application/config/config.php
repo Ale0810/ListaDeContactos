7124bff5b9a42d33ce4ebae220b87f7dd362560c
Ale0810 <alejandroverteche@gmail.com>
2024-10-29 08:19:14 -0300

    arreglado error en configuracion

--- ProyectoPreParcial/application/config/config.php
+++ ProyectoPreParcial/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ProyectoPreParcial/ProyectoPreParcial/';
+$config['base_url'] = 'http://localhost/ListaDeContactos/ProyectoPreParcial/';
 
 /*
 |--------------------------------------------------------------------------
