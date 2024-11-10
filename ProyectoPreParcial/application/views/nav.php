4399b009bfea7f5ae6fe7f0395a7b3cac88e8899
Ale0810 <alejandroverteche@gmail.com>
2024-11-10 11:07:43 -0300

    Item para listar contatos

--- ProyectoPreParcial/application/views/nav.php
+++ ProyectoPreParcial/application/views/nav.php
@@ -25,6 +25,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="<?=site_url("contactos/agregar")?>">Agregar Contacto</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?=site_url("contactos/listar")?>">Lista de Contactos</a>
+                </li>
             </ul>
         </div>
     </div>
